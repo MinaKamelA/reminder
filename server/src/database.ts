305792bf18dc32c522dcb700bfafb7cd22fedf63
MinaKamelA <developmentmina@gmail.com>
2022-09-07 01:22:22 +0200

    linting the file

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,22 +1,22 @@
-import dotenv from 'dotenv';
-import {Pool} from 'pg';
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
 
-dotenv.config();
+dotenv.config()
 
 const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_DB_TEST,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-    ENV,
-} = process.env;
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_DB_TEST,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  ENV
+} = process.env
 
 const client = new Pool({
-    host: POSTGRES_HOST,
-    database: (ENV === 'dev')?POSTGRES_DB:POSTGRES_DB_TEST,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
-});
+  host: POSTGRES_HOST,
+  database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_DB_TEST,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD
+})
 
-export default client;
+export default client
