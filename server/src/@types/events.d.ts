ffb0d8df165449b885ace133723083657a60dddf
MinaKamelA <developmentmina@gmail.com>
2022-09-07 17:54:32 +0200

    renaming types files names to conventions

--- server/src/@types/events.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-declare type Event = {
-  id?: number
-  event_name: string
-  event_description: string
-  user: number
-}
--- server/src/@types/options.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-declare type Option = {
-  id?: number
-  option_name: string
-  option_description: string
-  periodic: boolean
-  event: number
-}
