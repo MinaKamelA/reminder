77c9db40d7c0bceef3fd4d02dbdd36ddf64bfa91
MinaKamelA <developmentmina@gmail.com>
2022-09-07 20:19:07 +0200

    add token verification middleware function

---
+++ server/src/services/verifyToken.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+// get env variables
+dotenv.config()
+
+const {
+  TOKEN_SECRET
+} = process.env
+
+const verifyToken = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  try {
+    const authHeader = (req.headers.authorization) as string
+    const token = authHeader.split(' ')[1]
+    const decode = jwt.verify(token, TOKEN_SECRET as string) as jwt.JwtPayload
+    req.userId = decode.user[0].id.toString()
+    next()
+  } catch (error) {
+    res.status(401)
+    res.json('401: Unauthorized (Access denied)')
+  }
+}
+
+export default verifyToken
