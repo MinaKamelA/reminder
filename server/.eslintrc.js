8458f51d083bbca23fc8accf7f58a2f031cbcf88
MinaKamelA <developmentmina@gmail.com>
2022-09-11 20:39:11 +0200

    enforce semicolon rule in eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,26 +1,28 @@
 const conf = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'plugin:react/recommended',
-        'standard-with-typescript'
-    ],
-    overrides: [
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname
-    },
-    plugins: [
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
 
-    ],
-    rules: {
-        semi: 'off'
-    }
-}
-module.exports = conf
+  ],
+  rules: {
+    semi: [1, 'always'],
+    '@typescript-eslint/semi': [1, 'always'],
+    indent: [1, 2]
+  }
+};
+module.exports = conf;
