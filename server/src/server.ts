56fef9eec3e7381a6233388d2f85cc41b0367c7c
MinaKamelA <developmentmina@gmail.com>
2022-09-11 20:43:36 +0200

    add cookie-parser and use it

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,27 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import helmet from 'helmet'
-import dotenv from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 // getting env variables
-dotenv.config()
-const URL = process.env as unknown as string
+dotenv.config();
+const { URL } = process.env;
 // create app
-const app = express()
+const app = express();
 // middleware
-app.use(bodyParser.json())
-app.use(cors())
-app.use(helmet())
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(cookieParser());
 // initiate server
-const port = 5000
+const port = 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
-  console.log(`Please visit ${URL} to run your app`)
-})
+  console.log(`Server is running on port: ${port}`);
+  console.log(`Please visit ${URL as string} to run your app`);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
