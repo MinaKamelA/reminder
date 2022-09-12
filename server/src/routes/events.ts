19bc6cdfffb0096e606361761f83fa94ce67a60d
MinaKamelA <developmentmina@gmail.com>
2022-09-12 19:28:48 +0200

    add event options route

--- server/src/routes/events.ts
+++ server/src/routes/events.ts
@@ -10,7 +10,7 @@ events.get('/:id', verifyToken, controller.show);
 events.post('/', verifyToken, controller.create);
 events.delete('/:id', verifyToken, controller.destroy);
 events.put('/', verifyToken, controller.edit);
-
+// get event options
 events.get('/:id/options', verifyToken, eventOptions);
 
 export default events;
