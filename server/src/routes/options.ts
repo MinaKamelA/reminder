3d3844eab0780f01a31a97db281dd0b2149f4a32
MinaKamelA <developmentmina@gmail.com>
2022-09-12 19:38:09 +0200

    add option periods route

--- server/src/routes/options.ts
+++ server/src/routes/options.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as controller from '../handlers/options';
+import { optionPeriods } from '../handlers/periods';
 import verifyToken from '../services/verifyToken';
 
 const options = Router();
@@ -9,5 +10,7 @@ options.get('/:id', verifyToken, controller.show);
 options.post('/', verifyToken, controller.create);
 options.delete('/:id', verifyToken, controller.destroy);
 options.put('/', verifyToken, controller.edit);
+// get option periods
+options.get('/:id/periods', verifyToken, optionPeriods);
 
 export default options;
