4653e777a4f89831bab8bbdc122bbae040631300
MinaKamelA <developmentmina@gmail.com>
2022-09-12 19:26:06 +0200

    add event options route

--- server/src/routes/events.ts
+++ server/src/routes/events.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as controller from '../handlers/events';
+import { eventOptions } from '../handlers/options';
 import verifyToken from '../services/verifyToken';
 
 const events = Router();
@@ -9,5 +10,7 @@ events.get('/:id', verifyToken, controller.show);
 events.post('/', verifyToken, controller.create);
 events.delete('/:id', verifyToken, controller.destroy);
 events.put('/', verifyToken, controller.edit);
+
+events.get('/:id/options', verifyToken, eventOptions);
 
 export default events;
