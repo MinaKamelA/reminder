fa6bc9924cc905a3f723db137c920c329610fd30
MinaKamelA <developmentmina@gmail.com>
2022-09-21 18:03:56 +0200

    add eslint runtime configurations

---
+++ frontend/reminder/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+};
